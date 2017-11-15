4f3615c953078680e1ed3f6c50df497f2739915d
turmi05 <Miroslaw.Turski@ca.com>
2017-11-15 11:53:34 +0000

    Add bean name management
    Also clean up Eclipse files

--- application/src/main/java/multi/app/Application.java
+++ application/src/main/java/multi/app/Application.java
@@ -1,13 +1,33 @@
 package multi.app;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+        builder
+        .initializers(new CustomAppContextInitializer())
+//        .beanNameGenerator(new DefaultBeanNameGenerator())
+        .build().run(args);
+    }
+
+}
+
+class CustomAppContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Override
+    public void initialize(ConfigurableApplicationContext context) {
+        // Make sure that beans with the same name are not overridden, as it may lead to
+        // some inconsistencies given that the modules are auto configured.
+        if (context.getBeanFactory() instanceof DefaultListableBeanFactory) {
+            ((DefaultListableBeanFactory)context.getBeanFactory()).setAllowBeanDefinitionOverriding(false);
+        }
     }
 
 }
